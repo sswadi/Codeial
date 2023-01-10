4ef8f4bd8e7618db7c71265113ac432b1520f5ec
swati <s.swati.5293@gmail.com>
2023-01-10 12:15:29 +0530

    10th Jan'23

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 //const because we don't want to value of the variable to be overriden
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+const db = require('./config/mongoose');
+//used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
+// const MongoStore = require('connect-mongo')(session);
+
+//setting up the middleware that converts scss file to css and then feeds the browser with this file
+const sassMiddleware = require('node-sass-middleware');
+
+//setting to use import and export scss files
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+
+}));
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+//extract styles and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+app.use(session({
+    name: 'codeial',
+    secret: '',
+    saveUninitialzed: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000*60*100)
+    } 
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticated);
+
+// use express router
+app.use('/',require('./routes/index'));
 
 app.listen(port, function(err){
     if(err){
---
+++ routes/userP.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+
+const userProfile = require('../controllers/userProfile_controller');
+
+router.get('/profile', passport.checkAuthentication, userProfile.userProfile);
+
+
+router.get('/sign-up', userProfile.signUp);
+router.get('/sign-in', userProfile.signIn);
+
+router.post('/create',userProfile.create);
+
+//use passport as a middleware to authenticate
+router.post('/create-session', passport.authenticate('local',{failureRedirect: '/users/sign-in'},) , usersController.createSession);
+
+module.exports = router;
